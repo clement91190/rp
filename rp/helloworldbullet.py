8b517ca5a48936ca141c6a479e484c2ebe2e9717
clement91190 <clem.jambou@gmail.com>
2013-11-01 14:35:32 +0000

    multitask problems...

--- rp/helloworldbullet.py
+++ rp/helloworldbullet.py
@@ -33,12 +33,24 @@ np.setPos(0, 0, -2)
 world.attachRigidBody(node)
 
 
-# Boxes
-model = loader.loadModel('models/box.egg')
-model.setPos(-0.5, -0.5, -0.5)
-model.flattenLight()
-shape = BulletBoxShape(Vec3(0.5, 0.5, 0.5))
-for i in range(20):
+def define_model():
+    model = loader.loadModel('models/box.egg')
+    model.setPos(-0.5, -0.5, -0.5)
+    model.flattenLight()
+    return model
+
+
+def define_shape():
+    shape = BulletBoxShape(Vec3(0.5, 0.5, 0.5))
+    return shape
+
+
+model = define_model()
+shape = define_shape()
+
+
+def add_box(i):
+    # Boxes
     node = BulletRigidBodyNode('Box')
     node.setMass(1.0)
     node.addShape(shape)
@@ -46,6 +58,14 @@ for i in range(20):
     np.setPos(0, i*0.2 % 0.5, 2+i*1.1 )
     world.attachRigidBody(node)
     model.copyTo(np)
+
+
+for i in range(4):
+    add_box(i)
+
+
+def get_render():
+    return render
 
 
 # Update
@@ -56,7 +76,15 @@ def update(task):
  
 taskMgr.add(update, 'update')
 
-p = Process(target=app.run)
 
-p.start()
-app.run()
+def main():
+    while(True):
+        taskMgr.step()
+
+#p = Process(target=main)
+
+#p.start()
+
+print "coucou ca va ?"
+#p.join()
+#app.run()
